73053e0d3fddad427e2c0006af5047ca0b0416f7
Jannis Achstetter <kripton@kripserver.net>
2025-07-08 23:35:12 +0200

    feat: Reduce logging, cycle through all input registers of an Eastron SDM72D-M

---
+++ src/eastron-SDM72M.h
@@ -0,0 +1,75 @@
+#ifndef EASTRON_SDM72M_H
+#define EASTRON_SDM72M_H
+
+#include <stdint.h>
+
+uint16_t eastron_sdm72m_input_registers[] = {
+    0x0000, // Ph1 Voltage to Neutral,
+    0x0002, // Ph2 Voltage to Neutral,
+    0x0004, // Ph3 Voltage to Neutral,
+    0x0006, // Ph1 Current,
+    0x0008, // Ph2 Current,
+    0x000A, // Ph3 Current,
+    0x000C, // Ph1 Active Power,
+    0x000E, // Ph2 Active Power,
+    0x0010, // Ph3 Active Power,
+    0x0012, // Ph1 Apparent Power,
+    0x0014, // Ph2 Apparent Power,
+    0x0016, // Ph3 Apparent Power,
+    0x0018, // Ph1 Reactive Power,
+    0x001A, // Ph2 Reactive Power,
+    0x001C, // Ph3 Reactive Power,
+    0x001E, // Ph1 Power Factor,
+    0x0020, // Ph2 Power Factor,
+    0x0022, // Ph3 Power Factor,
+    0x002A, // Avg Voltage Phases to Neutral,
+    0x002E, // Avg Current,
+    0x0030, // Sum Phase Currents,
+    0x0034, // Total System power,
+    0x0038, // Total System VoltAmps,
+    0x003C, // Total System VAr,
+    0x003E, // Total System Power Factor,
+    0x0046, // Total System Frequency,
+    0x0048, // Total System Import Active Energy (since reset),
+    0x004A, // Total System Export Active Energy (since reset),
+    0x004C, // Total System Import VArh (since reset),
+    0x004E, // Total System Export VArh (since reset),
+    0x00C8, // Voltage Ph1 to Ph2,
+    0x00CA, // Voltage Ph2 to Ph3,
+    0x00CC, // Voltage Ph3 to Ph1,
+    0x00CE, // Avg Voltage Phases to Phases,
+    0x00E0, // Current Neutral,
+    0x0156, // Total kWh (Import + Export),
+    0x0158, // Total kVArh (Import + Export),
+
+    // Only in manual for SDM72M-V2:
+    0x0180, // Resetable total active energy (kWh),
+    0x0182, // Resetable total reactive energy (kVArh),
+    0x0184, // Resetable import active energy (kVAh),
+    0x0186, // Resetable export active energy (kVAh),
+    0x018C, // Net kWh (Import - Export),
+    0x0500, // Total import active power,
+    0x0502, // Total export active power,
+};
+
+uint16_t eastron_sdm72m_holding_registers[] = {
+    0x000A, // System type (1 = 1P2W, 3=3P4W)
+    0x000C, // Pulse width
+    0x000E, // Key Parameter Programming Authorization (0 = no, 1 = yes)
+    0x0012, // Parity and Stop bit (0=8N1, 1=8E1, 2=8O1, 3=8N2)
+    0x0014, // Modbus slave address (1..247)
+    0x0016, // Pulse constant (0=1000imp/kWh, 1=100imp/kWh, 2=10imp/kWh, 3=1imp/kWh)
+    0x0018, // Password
+    0x001C, // Baud rate (0=2400, 1=4800, 2=9600, 3=19200, 4=38400, 5=1200)
+    0x003A, // Automatic scroll display time (0..60)
+    0x003C, // Backlight time (0..121 with 0=always on and 121=always off)
+    0x0056, // Pulse 1 Energy type (1=import active, 2=total active, 4=export active)
+    0xF010, // 0003 = reset energy info
+
+    // Only in manual for SDM72M-V2:
+    0xFC00, // Serial number, 4byte
+    0xFC02, // Meter model code, 2byte
+    0xFC84, // Firmware version, 2byte
+};
+
+#endif // EASTRON_SDM72M_H
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ extern "C" {
     #include "pins.h"
     #include "picotool_binary_information.h"
     #include "crc_modbus.h"
+    #include "eastron-SDM72M.h"
 
     #include "uart_rx.pio.h"
     #include "uart_tx.pio.h"
@@ -145,15 +146,15 @@ int modbus_request(uint8_t slave_address, uint16_t function_code,
 
     request[7] = crc & 0xFF; // CRC low byte
     request[8] = (crc >> 8) & 0xFF; // CRC high byte
-    LOG("[UART TX] Sending request: ");
-    LOGHEX(9, request);
+    //LOG("[UART TX] Sending request: ");
+    //LOGHEX(9, request);
     // Send the request
     uart_tx_program_init(pio0, 0, offsetTx, 24, 25, 19200);
     for (size_t i = 0; i < 9; i++) {
         uart_tx_program_putc(pio0, 0, request[i]);
     }
     sleep_us(4760); // Wait for the data to be sent
-    LOG("[UART TX] Request sent, waiting for response...");
+    //LOG("[UART TX] Request sent, waiting for response...");
     // MUX our data pin to PIO1 for receiving
     uart_rx_program_init(pio1, 0, offsetRx, 24, 19200);
     // Wait for the response
@@ -162,15 +163,15 @@ int modbus_request(uint8_t slave_address, uint16_t function_code,
     uint16_t crc_received;
 
     for (size_t i = 0; i < response_buffer_size; i++) {
-        uint8_t c = uart_rx_program_getc(pio1, 0, 2);
+        uint8_t c = uart_rx_program_getc(pio1, 0, 10);
 
         if (i == 0 && c != slave_address) {
-            LOG("[UART RX] Slave address does not match. Expected: %02x, Got: %02x", slave_address, c);
+            //LOG("[UART RX] Slave address does not match. Expected: %02x, Got: %02x", slave_address, c);
             return -1; // Invalid response
         }
 
         if (i == 1 && c != function_code) {
-            LOG("[UART RX] Function code does not match. Expected: %02x, Got: %02x", function_code, c);
+            //LOG("[UART RX] Function code does not match. Expected: %02x, Got: %02x", function_code, c);
             return -1; // Invalid response
         }
 
@@ -183,7 +184,6 @@ int modbus_request(uint8_t slave_address, uint16_t function_code,
             crc_received = (c << 8); // High byte of CRC
         } else if (i == (4 + byte_count)) {
             crc_received |= c; // Low byte of CRC
-            LOG("[UART RX] Received CRC: %04x", crc_received);
             // Validate CRC
             uint16_t crc_calculated = crc_init();
             crc_calculated = crc_update(crc_calculated, response_buffer, 3 + byte_count);
@@ -193,17 +193,17 @@ int modbus_request(uint8_t slave_address, uint16_t function_code,
             crc_calculated = (crc_calculated >> 8) | (crc_calculated << 8);
 
             if (crc_calculated != crc_received) {
-                LOG("[UART RX] CRC mismatch! Expected: %04x, Got: %04x", crc_calculated, crc_received);
+                //LOG("[UART RX] CRC mismatch! Expected: %04x, Got: %04x", crc_calculated, crc_received);
                 return -1; // Invalid response
             }
-            LOG("[UART RX] CRC valid.");
+            //LOG("[UART RX] CRC valid.");
             return bytes_received; // Return the number of bytes received
             break; // Exit loop after receiving the full response
         }
 
         response_buffer[i] = c;
         bytes_received++;
-        LOG("[UART RX] Got byte: %02x", c);
+        //LOG("[UART RX] Got byte: %02x", c);
     }
 
     return bytes_received;
@@ -213,59 +213,39 @@ int modbus_request(uint8_t slave_address, uint16_t function_code,
 //        Config reset (Button reading and state machine but NOT flash)
 void core1_tasks() {
     int state;
+    int retVal;
+    uint8_t curModbusReg = 0xff; // Current Modbus register to read
     uint8_t byteArr[255];
+
+    uint8_t numModbusReg = sizeof(eastron_sdm72m_input_registers) / sizeof(eastron_sdm72m_input_registers[0]);
 
     while (true) {
         //LOG("Core1 idling about ...");
 
-        // First byte is length-1, follow by data.
-        // do NOT use _puts() here since it will break on \x00
+        curModbusReg++;
+        if (curModbusReg >= numModbusReg) {
+            LOG("Core1: All registers read, resetting to 0");
+            curModbusReg = 0;
+        }
 
-        modbus_request(0x01, 0x04, 0x0034, 0x0002, byteArr, sizeof(byteArr));
+        retVal = modbus_request(0x01, 0x04, eastron_sdm72m_input_registers[curModbusReg], 0x0002, byteArr, sizeof(byteArr));
 
-        /*
-        // MUX our data pin to PIO0
-        uart_tx_program_init(pio0, 0, offsetTx, 24, 25, 19200);
+        if (retVal > 0) {
+            // Convert and print the received data
+            float val;
+            memcpy(&val, byteArr+3, sizeof(float));
 
-        // Send the data
-        uart_tx_program_putc(pio0, 0, '\x07'); // 8 byte in total
-        uart_tx_program_putc(pio0, 0, '\x01'); // slave address
-        uart_tx_program_putc(pio0, 0, '\x04'); // Function code: Read input registers
-        uart_tx_program_putc(pio0, 0, '\x00'); // Offset, high byte
-        uart_tx_program_putc(pio0, 0, '\x34'); // Offset, low byte
-        uart_tx_program_putc(pio0, 0, '\x00'); // Word count, high byte
-        uart_tx_program_putc(pio0, 0, '\x02'); // Word count, low byte
-        uart_tx_program_putc(pio0, 0, '\x30'); // CRC
-        uart_tx_program_putc(pio0, 0, '\x05'); // CRC
-        
-        sleep_us(4760); // Wait for the data to be sent
+            // Reverse the 4 bytes of val
+            uint8_t* valBytes = (uint8_t*)&val;
+            valBytes[0] = byteArr[6];
+            valBytes[1] = byteArr[5];
+            valBytes[2] = byteArr[4];
+            valBytes[3] = byteArr[3];
+            LOG("Register %04x: %f", eastron_sdm72m_input_registers[curModbusReg], val);
+        }
 
-        // MUX our data pin to PIO1
-        uart_rx_program_init(pio1, 0, offsetRx, 24, 19200);
-
-        // Wait for the response
-        char c;
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        c = uart_rx_program_getc(pio1, 0, 10);
-        LOG("[UART RX] Got byte: %02x", c);
-        */
-
-        sleep_ms(2000);
+        // 20ms works as well, 10ms crashes the SDM72M
+        sleep_ms(40);
     }
 
     /*
